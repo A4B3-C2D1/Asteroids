637eecb5029684d0931adaa257cc798e6a5a3c21
A4B3-C2D1 <21JE0563@iitism.ac.in>
2024-12-18 17:09:29 +0000

    2) 60 FPS

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from constants import *
 
 def main():
     pygame.init()
+    clock=pygame.time.Clock()
+    dt=0
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -14,6 +16,8 @@ def main():
                 return
         screen.fill(black)
         pygame.display.flip()
+        delta=clock.tick(60)
+        dt=delta/1000
 
 
 if __name__ == "__main__":
